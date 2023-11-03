244f25b439474fafa14b3365a764fd436cea35fb
Nytra <samueltscott@gmail.com>
2023-11-03 17:54:19 +0000

    Upload the mod files

---
+++ LogFlush/LogFlush.cs
@@ -0,0 +1,32 @@
+using HarmonyLib;
+using ResoniteModLoader;
+using FrooxEngine;
+
+namespace LogFlush
+{
+	public class LogFlush : ResoniteMod
+	{
+		public override string Name => "LogFlush";
+		public override string Author => "Nytra";
+		public override string Version => "1.0.0";
+		public override string Link => "https://github.com/Nytra/ResoniteLogFlush";
+		public override void OnEngineInit()
+		{
+			Harmony harmony = new Harmony("owo.Nytra.LogFlush");
+			harmony.PatchAll();
+		}
+
+		[HarmonyPatch(typeof(Userspace), "OnCommonUpdate")]
+		class LogFlushPatch
+		{
+			public static void Postfix()
+			{
+                if (Engine.Current.InputInterface.GetKeyDown(Key.F5))
+				{
+					Msg("Flushing LogStream...");
+					FrooxEngineBootstrap.LogStream.Flush();
+				}
+            }
+		}
+	}
+}
